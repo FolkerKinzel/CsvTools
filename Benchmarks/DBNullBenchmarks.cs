12c0b0846b5ca45b9f12aa314db92eabd916fd11
Folker Kinzel <fkinzel@freenet.de>
2021-03-30 12:22:53 +0200

    Benchmarks

---
+++ Benchmarks/DBNullBenchmarks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace Benchmarks
+{
+    public class DBNullBenchmarks
+    {
+        private readonly object _o = DBNull.Value;
+
+        [Benchmark]
+        public bool ConvertIsDBNull() => Convert.IsDBNull(_o);
+
+        [Benchmark]
+        public bool IsDBNull() => _o is DBNull;
+
+        [Benchmark]
+        public bool EqualSign() => _o == DBNull.Value;
+
+        [Benchmark]
+        public bool EqualsMethod() => DBNull.Value.Equals(_o);
+
+        [Benchmark]
+        public bool ReferenceEquals() => object.ReferenceEquals(_o, DBNull.Value);
+
+    }
+}
---
+++ Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    internal class Program
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0059:Unnötige Zuweisung eines Werts.", Justification = "<Ausstehend>")]
+        private static void Main()
+        {
+            Summary summary = BenchmarkRunner.Run<DBNullBenchmarks>();
+        }
+    }
+}
