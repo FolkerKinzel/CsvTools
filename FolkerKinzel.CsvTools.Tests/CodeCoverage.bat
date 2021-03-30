97014ce841fdcf18c84b5c7bb023268de7fd1a79
Folker Kinzel <fkinzel@freenet.de>
2021-03-30 15:48:23 +0200

    Code coverage

--- FolkerKinzel.CsvTools.Tests/CodeCoverage.bat
+++ FolkerKinzel.CsvTools.Tests/CodeCoverage.bat
@@ -1,2 +1,2 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./reports/ /p:ExcludeByAttribute="Obsolete%2cGeneratedCodeAttribute%2cCompilerGeneratedAttribute" /p:SkipAutoProps=true /p:DoesNotReturnAttribute="DoesNotReturnAttribute"
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./reports/ /p:ExcludeByAttribute=\"Obsolete,GeneratedCode,CompilerGenerated\" /p:SkipAutoProps=true /p:DoesNotReturnAttribute="DoesNotReturnAttribute"
 reportgenerator -reports:./reports/coverage.net5.0.cobertura.xml -targetdir:./reports/html/
