b8570ba04cf4a853d92969a89036210d2fcc9124
Folker Kinzel <fkinzel@freenet.de>
2022-01-20 23:38:06 +0100

    nuget package

--- src/FolkerKinzel.CsvTools.Tests/CsvReaderTests.cs
+++ src/FolkerKinzel.CsvTools.Tests/CsvReaderTests.cs
@@ -20,7 +20,7 @@ namespace FolkerKinzel.CsvTools.Tests
         {
             const string testCsv =
                 "Spalte \"1\",," + "\r\n" +
-                ",Spalte \"2\",";
+                ",Spalte \"2\"," + "\r\n";
 
             using var stringReader = new StringReader(testCsv);
             using var csvReader = new CsvReader(stringReader, hasHeaderRow: false);
