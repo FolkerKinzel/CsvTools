c93fe1f82e0b80707f6bf28ae5efc383f1449f7b
Folker Kinzel <fkinzel@freenet.de>
2022-02-05 17:30:58 +0100

    Split repositories

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -10,7 +10,7 @@ namespace Benchmarks
         private static void Main()
         {
             //Summary summary = BenchmarkRunner.Run<DBNullBenchmarks>();
-            Summary summary = BenchmarkRunner.Run<AccessBenchmark>();
+            //Summary summary = BenchmarkRunner.Run<AccessBenchmark>();
 
         }
     }
